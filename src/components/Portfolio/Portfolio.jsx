7c38b97d591df2ac06788b41daa54178aa4d1f87
bodymostafa1 <139517580+bodymostafa1@users.noreply.github.com>
2023-09-13 17:08:58 -0700

    portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,76 +1,85 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import style from "./Portfolio.module.css"
-import img1 from "./poert1.png"
-import img2 from "./port2.png"
-import img3 from "./port3.png"
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import style from "./Portfolio.module.css";
+import img1 from "./poert1.png";
+import img2 from "./port2.png";
+import img3 from "./port3.png";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
 export default function Portfolio() {
-  
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedImage(null);
+  };
+
+  const handleShowModal = (image) => {
+    setSelectedImage(image);
+    setShowModal(true);
+  };
+
+  const images = [img1, img2, img3, img1, img2, img3];
+
   return (
     <>
-    <div className={`w-100 h-100 d-flex justify-content-center align-items-center position-fixed`}>
-      <img src={""} alt="" />
-    </div>
-      <div className={`container home ${style.textcolor} text-center pt-5 pb-5`}>
-        <h2 className='fs-1'> PORTFOLIO COMPONENT</h2>
-        <div className='d-flex justify-content-center align-items-center pb-3 pt-2'>
+      <div
+        className={`w-100 h-100 d-flex justify-content-center align-items-center position-fixed ${style.imageshow}`}
+      >
+        {selectedImage && (
+          <Modal
+            show={showModal}
+            onHide={handleCloseModal}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Image Preview</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <img src={selectedImage} alt="Selected" className="w-100" />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleCloseModal}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        )}
+      </div>
+      <div
+        className={`container home ${style.textcolor} text-center pt-5 pb-5`}
+      >
+        <h2 className="fs-1"> PORTFOLIO COMPONENT</h2>
+        <div className="d-flex justify-content-center align-items-center pb-3 pt-2">
           <div className={`${style.line} ${style.textcolor}`}></div>
-          <FontAwesomeIcon icon={faStar} className='mx-3' />
+          <FontAwesomeIcon icon={faStar} className="mx-3" />
           <div className={`${style.line}`}></div>
         </div>
+
         <div className="row gy-5 gx-5">
-          <div className="col-md-4 ">
-            <div className='position-relative ' >
-              <div className={`${style.overlay} d-flex justify-content-center align-items-center text-white`}>
-                <FontAwesomeIcon icon={faPlus} className={`${style.big_icon}`} />
+          {images.map((image, index) => (
+            <div className="col-md-4" key={index}>
+              <div className="position-relative">
+                <div
+                  className={`${style.overlay} d-flex justify-content-center align-items-center text-white`}
+                  onClick={() => handleShowModal(image)}
+                >
+                  <FontAwesomeIcon
+                    icon={faPlus}
+                    className={`${style.big_icon}`}
+                  />
+                </div>
+                <img src={image} alt="" className="w-100 rounded-3" />
               </div>
-              <img src={img1} alt="" className='w-100 rounded-3' />
             </div>
-          </div>
-          <div className="col-md-4 ">
-            <div className='position-relative xyz'>
-              <div className={`${style.overlay} d-flex justify-content-center align-items-center text-white`}>
-                <FontAwesomeIcon icon={faPlus} className={`${style.big_icon}`} />
-              </div>
-              <img src={img2} alt="" className='w-100 rounded-3'/>
-            </div>
-          </div>
-          <div className="col-md-4 ">
-            <div className='position-relative xyz'>
-              <div className={`${style.overlay} d-flex justify-content-center align-items-center text-white`}>
-                <FontAwesomeIcon icon={faPlus} className={`${style.big_icon}`} />
-              </div>
-              <img src={img3} alt="" className='w-100 rounded-3' />
-            </div>
-          </div>
-          <div className="col-md-4 ">
-            <div className='position-relative xyz'>
-              <div className={`${style.overlay} d-flex justify-content-center align-items-center text-white`}>
-                <FontAwesomeIcon icon={faPlus} className={`${style.big_icon}`} />
-              </div>
-              <img src={img1} alt="" className='w-100 rounded-3' />
-            </div>
-          </div>
-          <div className="col-md-4 ">
-            <div className='position-relative xyz'>
-              <div className={`${style.overlay} d-flex justify-content-center align-items-center text-white`}>
-                <FontAwesomeIcon icon={faPlus} className={`${style.big_icon}`} />
-              </div>
-              <img src={img2} alt="" className='w-100 rounded-3' />
-            </div>
-          </div>
-          <div className="col-md-4 ">
-            <div className='position-relative xyz'>
-              <div className={`${style.overlay} d-flex justify-content-center align-items-center text-white`}>
-                <FontAwesomeIcon icon={faPlus} className={`${style.big_icon}`} />
-              </div>
-              <img src={img3} alt="" className='w-100 rounded-3' />
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
-  )
+  );
 }
